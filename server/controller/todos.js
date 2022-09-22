3ad4a83e50ebfb49fa58179e77f7d49ec666ba5b
unknown <git.markan2nypascual@gmail.com>
2022-09-22 15:42:21 +0800

    Add get todos controller

--- server/controller/todos.js
+++ server/controller/todos.js
@@ -13,4 +13,13 @@ router.post("/api/todos", async (req, res) => {
   }
 });
 
+router.get("/api/todos", async (req, res) => {
+  try {
+    const allData = await Todo.find({});
+    res.status(200).json(allData);
+  } catch (error) {
+    res.json(error);
+  }
+});
+
 module.exports = router;
